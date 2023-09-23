88224d9ac5b3c1e39c8e01a86dc5d1e01862ae16
Kartikeya Dubey <KD@gmail.com>
2023-09-23 16:28:36 +0530

    tuned autoencoder

--- AutoencoderV1.py
+++ AutoencoderV1.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 import numpy as np
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Cropping2D
 from keras.models import Sequential, Model, load_model
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+from scipy.stats import norm
 
 import matplotlib.pyplot as plt
 
@@ -30,6 +33,12 @@ abp_mean_data = np.load("numpy_saved_data/abp_mean_data.npy")
 print("Loaded data")
 
 combined_data = np.stack((spo2_data, pulse_data, hr_data, resp_data, abp_sys_data, abp_dia_data, abp_mean_data), axis=-1)
+
+contains_zero = np.any(combined_data == 0, axis=1)
+
+# Use boolean indexing to remove rows containing 0
+combined_data = combined_data[~contains_zero]
+print(combined_data.shape)
 
 scaler = MinMaxScaler()
 combined_data = scaler.fit_transform(combined_data)
@@ -86,20 +95,35 @@ cropped = Cropping2D(cropping=((0, 1), (0, 1)))(decoded)
 
 autoencoder = Model(inputs=input_layer, outputs=cropped)
 
-autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+autoencoder.compile(optimizer='adam', loss='mean_absolute_error')  # binary_crossentropy
+
+# Apply regularization and other strategies
+# 1. Early Stopping
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+# 2. Reduce Learning Rate on Plateau
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
+
 
 print("created model")
 
-autoencoder.fit(X_train, X_train, epochs=2, batch_size=256, shuffle=True, validation_data=(X_test, X_test))
+# autoencoder.fit(X_train, X_train,
+#                 epochs=25,
+#                 batch_size=128,
+#                 shuffle=True, 
+#                 validation_data=(X_test, X_test),
+#                 callbacks=[early_stopping, reduce_lr]
+#                 )
 
-autoencoder.summary()
 
-autoencoder.save("models/autoencoderV1-2.h5")
 
+# autoencoder.summary()
+
+# autoencoder.save("models/autoencoderV2.h5")
 
 print("Model Saved")    # To load: tf.keras.models.load_model(model_path)
 
-# autoencoder = load_model("models/autoencoderV1.h5")
+autoencoder = load_model("models/autoencoderV2.h5")
 
 Y_val = autoencoder.predict(X_val)
 
@@ -111,19 +135,28 @@ temp = Y_val.shape[0] * Y_val.shape[1]
 Y_val = Y_val.reshape((temp, 7))
 X_val = X_val.reshape((temp, 7))
 
-val_MSE = mean_squared_error(X_val, Y_val)
-val_MAE = mean_absolute_error(X_val, Y_val)
+INDEX_TO_CHECK = 0
 
+abs_diff = np.abs(Y_val[:, INDEX_TO_CHECK] - X_val[:, INDEX_TO_CHECK])
+
+val_MSE = mean_squared_error(X_val[:, INDEX_TO_CHECK], Y_val[:, INDEX_TO_CHECK])
+val_MAE = mean_absolute_error(X_val[:, INDEX_TO_CHECK], Y_val[:, INDEX_TO_CHECK])
+error_SD = np.std(Y_val[:, INDEX_TO_CHECK] - X_val[:, INDEX_TO_CHECK])
+
+ANOMALIES = np.where(abs_diff > error_SD)[INDEX_TO_CHECK]
+
+print(f"Number of anomalies: {ANOMALIES.shape[0]}")
 print(f"Mean Squared Error (MSE) on Validation Set: {val_MSE:.4f}")
 print(f"Mean Absolute Error (MAE) on Validation Set: {val_MAE:.4f}")
+print(f"Standard Deviation of Error on Validation Set: {error_SD:.4f}")
 
-plt.plot(X_val[:, 0], label="x")
-plt.plot(Y_val[:, 0], label="y")
+
+plt.plot(X_val[:ANOMALIES[100], INDEX_TO_CHECK], label="x")
+plt.plot(Y_val[:ANOMALIES[100], INDEX_TO_CHECK], label="y")
+plt.plot(ANOMALIES[:100], X_val[ANOMALIES[:100], INDEX_TO_CHECK], 'ro', label='Difference > Threshold')
 
 plt.grid()
-# plt.ylim(-50, 400)
 plt.legend(loc="upper right")
-plt.title('ABP')
 
 plt.ioff()
 plt.tight_layout()
